d922d1448e7d7932a4fc1d91581e7b54a2242f58
Kyle Kodani <poolioman@sbcglobal.net>
2016-02-18 17:18:43 -0800

    [Feature] Added a basic server with routing skeleton

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/../client/'));
+
+// route for getting search results
+app.get('/api/search', function (req, res) {
+	console.dir(req);
+  res.send('GET request for search');
+});
+
+// route for everything else 
+app.get('/*', function (req, res) {
+  res.send('404 NOT FOUND');
+});
+
+module.exports = app;
