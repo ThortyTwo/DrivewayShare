189ab42af83057db3d69ec4e25eb94a5726760d8
Kasra Jahani <Kasra.Jahani@gmail.com>
2016-02-23 19:04:25 -0800

    [Feature] Implemented client side authentication

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,9 +1,11 @@
-app.controller("HomeController", function($scope, Listings) {
+app.controller("HomeController", function($scope, Listings, Auth, $location, $window) {
 	
+	$scope.user = {};
 	$scope.data = {};
 	$scope.search = "";
 	$scope.expand = false;
 	
+
 	$scope.getSearch = function (searchInput) {
 		Listings.getListings(searchInput)
 		.then(function (searchResult) {
@@ -17,4 +19,14 @@ app.controller("HomeController", function($scope, Listings) {
 		$scope.expand = !$scope.expand;
 	};
 
+	$scope.signin = function () {
+		Auth.signin($scope.user)
+		.then(function (token) {
+			$window.localStorage.setItem("authentication", token);
+			$location.path("/user");
+		})
+		.catch(function (error) {
+			console.error(error);
+		});
+	};
 });
---
+++ client/app/signup/signup.js
@@ -0,0 +1,15 @@
+app.controller("SignupController", function ($scope, Auth, $location, $window){
+
+	$scope.user = {};
+
+	$scope.signup = function () {
+		Auth.signup($scope.user)
+		.then(function (token){
+			$window.localStorage.setItem("authentication", token);
+			$location.path("/user");
+		})
+		.catch(function (error) {
+			console.error(error);
+		})
+	}
+})
