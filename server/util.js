665538e315989616d3e9d36c0f1fb7589bb36a60
Kyle Kodani <poolioman@sbcglobal.net>
2016-02-25 15:27:49 -0800

    [Feature] Attached currenlty logged in user info to the making a new listing process

--- server/util.js
+++ server/util.js
@@ -1,3 +1,11 @@
+var jwt = require("jwt-simple");
+
+
 module.exports = {
-	days: ["M", "Tu", "W", "Th", "F", "Sa", "Su"]
+	days: ["M", "Tu", "W", "Th", "F", "Sa", "Su"],
+
+	getCurrentUserID: function(token) {
+		var currentUser = jwt.decode(token, "secret");
+		return currentUser.id;
+	}
 }
